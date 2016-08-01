529a929d99091bf8d367e0792770864acb4954d3
YuggY <YuggY@users.noreply.github.com>
2016-08-01 22:07:11 +0100

    Created post install

---
+++ mint-post-install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash +x
+# Help at https://community.linuxmint.com/tutorial/view/2026
+
+export LOCATION="sdc"
+export AES_SIZE="256"
+export LVM_NAME="lvm"
+export LVM_SWAP="3G"
+export LVM_ROOT="17G"
+
+clear
+echo -e "
+Current Config:
+  LOCATION=${LOCATION}
+  AES_SIZE=${AES_SIZE}
+  LVM_NAME=${LVM_NAME}
+  LVM_SWAP=${LVM_SWAP}
+  LVM_ROOT=${LVM_ROOT}
+"
+lsblk
+echo -e "
+Edit this file and CHECK that all the install suit your needs!
+"
+read -srn1 k
+
+sudo mount        /dev/mapper/${LVM_NAME}-root /mnt
+sudo mount --bind /dev                         /mnt/dev
+sudo mount --bind /dev/pts                     /mnt/dev/pts
+sudo mount --bind /sys                         /mnt/sys
+sudo mount --bind /proc                        /mnt/proc
+sudo mount --bind /run                         /mnt/run
+
+sudo dd bs=1024 count=4 if=/dev/urandom of=/mnt/.key
+sudo chmod 000 /mnt/.key
+sudo chmod -R g-rwx,o-rwx /mnt/boot
+sudo cryptsetup luksAddKey /dev/${LOCATION}1 /mnt/.key
+
+echo "cp /.key \"\${DESTDIR}\"" | sudo tee -a /mnt/etc/initramfs-tools/hooks/crypto_keyfile
+sudo chmod +x /mnt/etc/initramfs-tools/hooks/crypto_keyfile
+
+echo "crypt UUID=`sudo blkid -s UUID -o value /dev/${LOCATION}1` luks,keyscript=/bin/cat" | sudo tee -a /mnt/etc/crypttab
+
+sudo chroot /mnt locale-gen --purge --no-archive
+sudo chroot /mnt update-initramfs -u
+
+sudo sed -i.bak 's/GRUB_HIDDEN_TIMEOUT=0/#GRUB_HIDDEN_TIMEOUT=0/' /mnt/etc/default/grub
+sudo sed -i '10a GRUB_ENABLE_CRYPTODISK=y' /mnt/etc/default/grub
+sudo sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cryptdevice=\/dev\/'"${LOCATION}"'1:crypt"/' /mnt/etc/default/grub
+
+sudo chroot /mnt update-grub
+sudo chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+sudo chroot /mnt grub-install /dev/${LOCATION}
+
+sudo umount /mnt/proc /mnt/dev/pts /mnt/dev /mnt/sys /mnt/run /mnt
